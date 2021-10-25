eb479b3ae962be482d3c6c4580b79e9fd22b0bfa
willian156 <willian1561@outlook.com>
2021-10-25 00:08:10 -0300

    Controller Vagas 90% funcionando perfeitamente, apenas o 'edit' não funcionando

--- Rh_Web_app/Controllers/VagasController.cs
+++ Rh_Web_app/Controllers/VagasController.cs
@@ -12,62 +12,144 @@ namespace Rh_Web_app.Controllers
 {
     public class VagasController : Controller
     {
+
+        [HttpGet]
         public async Task<ActionResult> Index()
         {
             List<Vagas> VgaInfo = new List<Vagas>();
 
-                //mandando o request para achar os recursos de GetVaga usando HttpClient
-                HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            //mandando o request para achar os recursos de GetVaga usando HttpClient
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
 
-                //Checando a resposta se obteve sucesso ou não
-                if (Res.IsSuccessStatusCode)
-                {
-                    //Armazenando os detalhes da resposta recebidas pela API
-                    var VgaResponse = Res.Content.ReadAsStringAsync().Result;
+            //Checando a resposta se obteve sucesso ou não
+            if (Res.IsSuccessStatusCode)
+            {
+                //Armazenando os detalhes da resposta recebidas pela API
+                var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
-                    //Desserializando a resposta e a armazenando na lista de Vagas
-                    VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
-                }
-
-                //Retornando a lista de vagas para a view
-                return View(VgaInfo);
+                //Desserializando a resposta e a armazenando na lista de Vagas
+                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
             }
+
+            //Retornando a lista de vagas para a view
+            return View(VgaInfo);
         }
 
-        public ActionResult NovaVaga()
-        {
-            return View();
-        }
-        public async Task<ActionResult> NovaVaga()
+        public async Task<ActionResult<Vagas>> GetById(int id)
         {
             List<Vagas> VgaInfo = new List<Vagas>();
-            using (var client = new HttpClient())
+
+            Vagas vgEdit = new Vagas();
+
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+
+            if (Res.IsSuccessStatusCode)
             {
-                //Passando a url base do serviço
-                client.BaseAddress = new Uri(Baseurl);
-                client.DefaultRequestHeaders.Clear();
+                //Armazenando os detalhes da resposta recebidas pela API
+                var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
-                //Definindo o data format do request
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                //mandando o request para achar os recursos de GetVaga usando HttpClient
-                HttpResponseMessage Res = await client.GetAsync("api/Vagas");
-
-                //Checando a resposta se obteve sucesso ou não
-                if (Res.IsSuccessStatusCode)
+                //Desserializando a resposta e a armazenando na lista de Vagas
+                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+            }
+            foreach (Vagas item in VgaInfo)
+            {
+                if (id == item.id_vaga)
                 {
-                    //Armazenando os detalhes da resposta recebidas pela API
-                    var VgaResponse = Res.Content.ReadAsStringAsync().Result;
-
-                    //Desserializando a resposta e a armazenando na lista de Vagas
-                    VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                    vgEdit = item;
                 }
             }
 
-            foreach( Vagas item in VgaInfo)
-            {
-                if(!item.id_vaga.Equals(VgaInfo.))
-            }
+            return vgEdit;
+
         }
-    }
+
+
+        public ActionResult Create(int? id)
+        {
+            if(id != null)
+            {              
+                HttpResponseMessage Res = GlobalVariables.WebApiClient.GetAsync("api/Vagas" + id.ToString()).Result;
+                return View(Res.Content.ReadAsAsync<Vagas>().Result);
+            }
+            else
+            {
+                return View(new Vagas());
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult Create(Vagas item)
+        {
+            var postTask = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Vagas", item);
+            postTask.Wait();
+
+            var result = postTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+            return View(item);
+        }
+
+        public async Task<ActionResult<Vagas>> Edit(int id)
+        {
+            List<Vagas> VgaInfo = new List<Vagas>();
+
+            Vagas vgEdit = new Vagas();
+
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+
+            if (Res.IsSuccessStatusCode)
+            {
+                //Armazenando os detalhes da resposta recebidas pela API
+                var VgaResponse = Res.Content.ReadAsStringAsync().Result;
+
+                //Desserializando a resposta e a armazenando na lista de Vagas
+                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+            }
+            foreach (Vagas item in VgaInfo)
+            {
+                if (id == item.id_vaga)
+                {
+                    vgEdit = item;
+                    break;
+                }
+            }
+
+            return View(vgEdit);
+        }
+       
+
+        public async Task<ActionResult<Vagas>> Save(Vagas item)
+        {
+            var res = GlobalVariables.WebApiClient.PutAsJsonAsync("api/Vagas", item);
+            res.Wait();
+
+            var result = res.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+            return View(Edit(item.id_vaga));  
+        }
+
+
+        public async Task<ActionResult<Vagas>> Delete(int id) 
+        {
+            string urlDel = "api/Vagas/";
+            urlDel = urlDel + id.ToString();
+            var res = await GlobalVariables.WebApiClient.DeleteAsync(urlDel);
+
+
+            return RedirectToAction("Index");
+        }
+
+
+    }  
 }
+
--- Rh_Web_app/GlobalVariables.cs
+++ Rh_Web_app/GlobalVariables.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Rh_Web_app
 {
-    public class GlobalVariables
+    public static class GlobalVariables
     {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+            //instanciando a variável http com um url
+            WebApiClient.BaseAddress = new Uri("httpS://localhost:44305/");
+
+            //limpando e setando o tipo de recursos que a variável vai receber
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
