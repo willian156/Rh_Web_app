f7b3e80ae3f429cc8e45a6793653b75362d842d1
willian156 <willian1561@outlook.com>
2021-10-25 17:59:56 -0300

    Feita algumas alterações

--- Rh_Web_app/Controllers/ConhecimentosController.cs
+++ Rh_Web_app/Controllers/ConhecimentosController.cs
@@ -5,21 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Rh_Web_app.Controllers
 {
-    public class VagasController : Controller
+    public class ConhecimentosController : Controller
     {
-
         [HttpGet]
         public async Task<ActionResult> Index()
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Conhecimentos> VgaInfo = new List<Conhecimentos>();
 
-            //mandando o request para achar os recursos de GetVaga usando HttpClient
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            //mandando o request para achar os recursos de GetCurriculos usando HttpClient
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Conhecimentos");
 
             //Checando a resposta se obteve sucesso ou não
             if (Res.IsSuccessStatusCode)
@@ -28,20 +26,20 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Conhecimentos>>(VgaResponse);
             }
 
             //Retornando a lista de vagas para a view
             return View(VgaInfo);
         }
 
-        public async Task<ActionResult<Vagas>> GetById(int id)
+        public async Task<ActionResult<Conhecimentos>> GetById(int id)
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Conhecimentos> VgaInfo = new List<Conhecimentos>();
 
-            Vagas vgEdit = new Vagas();
+            Conhecimentos vgEdit = new Conhecimentos();
 
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Conhecimentos");
 
             if (Res.IsSuccessStatusCode)
             {
@@ -49,11 +47,11 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Conhecimentos>>(VgaResponse);
             }
-            foreach (Vagas item in VgaInfo)
+            foreach (Conhecimentos item in VgaInfo)
             {
-                if (id == item.id_vaga)
+                if (id == item.id_Conhecimentos)
                 {
                     vgEdit = item;
                 }
@@ -66,12 +64,12 @@ namespace Rh_Web_app.Controllers
 
         public ActionResult Create()
         {
-                return View(); 
+            return View();
         }
         [HttpPost]
-        public ActionResult Create(Vagas item)
+        public ActionResult Create(Conhecimentos item)
         {
-            var postTask = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Vagas", item);
+            var postTask = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Conhecimentos", item);
             postTask.Wait();
 
             var result = postTask.Result;
@@ -84,13 +82,13 @@ namespace Rh_Web_app.Controllers
             return View(item);
         }
 
-        public async Task<ActionResult<Vagas>> Edit(int id)
+        public async Task<ActionResult<Conhecimentos>> Edit(int id)
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Conhecimentos> VgaInfo = new List<Conhecimentos>();
 
-            Vagas vgEdit = new Vagas();
+            Conhecimentos vgEdit = new Conhecimentos();
 
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Conhecimentos");
 
             if (Res.IsSuccessStatusCode)
             {
@@ -98,11 +96,11 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Conhecimentos>>(VgaResponse);
             }
-            foreach (Vagas item in VgaInfo)
+            foreach (Conhecimentos item in VgaInfo)
             {
-                if (id == item.id_vaga)
+                if (id == item.id_Conhecimentos)
                 {
                     vgEdit = item;
                     break;
@@ -111,11 +109,11 @@ namespace Rh_Web_app.Controllers
 
             return View(vgEdit);
         }
-       
 
-        public async Task<ActionResult<Vagas>> Save(Vagas item)
+
+        public async Task<ActionResult<Conhecimentos>> Save(Conhecimentos item)
         {
-            var res = GlobalVariables.WebApiClient.PutAsJsonAsync("api/Vagas", item);
+            var res = GlobalVariables.WebApiClient.PutAsJsonAsync("api/Conhecimentos", item);
             res.Wait();
 
             var result = res.Result;
@@ -126,27 +124,27 @@ namespace Rh_Web_app.Controllers
             }
 
             ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-            return View(Edit(item.id_vaga));  
+            return View(Edit(item.id_Conhecimentos));
         }
 
 
-        public async Task<ActionResult<Vagas>> Delete(int id) 
+        public async Task<ActionResult<Conhecimentos>> Delete(int id)
         {
-            string urlDel = "api/Vagas/";
+            string urlDel = "api/Conhecimentos/";
             urlDel = urlDel + id.ToString();
             var res = await GlobalVariables.WebApiClient.DeleteAsync(urlDel);
 
 
             return RedirectToAction("Index");
         }
-        
-        public async Task<ActionResult<Vagas>> Details(int id)
+
+        public async Task<ActionResult<Conhecimentos>> Details(int id)
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Conhecimentos> VgaInfo = new List<Conhecimentos>();
 
-            Vagas vgView = new Vagas();
+            Conhecimentos vgView = new Conhecimentos();
 
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Curriculos");
 
             if (Res.IsSuccessStatusCode)
             {
@@ -154,11 +152,11 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Conhecimentos>>(VgaResponse);
             }
-            foreach (Vagas item in VgaInfo)
+            foreach (Conhecimentos item in VgaInfo)
             {
-                if (id == item.id_vaga)
+                if (id == item.id_Conhecimentos)
                 {
                     vgView = item;
                 }
@@ -166,8 +164,5 @@ namespace Rh_Web_app.Controllers
 
             return View(vgView);
         }
-
-
-    }  
+    }
 }
-
--- Rh_Web_app/Controllers/CurriculosController.cs
+++ Rh_Web_app/Controllers/CurriculosController.cs
@@ -1,25 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Rh_Web_app.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Rh_Web_app.Controllers
 {
-    public class VagasController : Controller
+    public class CurriculosController : Controller
     {
-
         [HttpGet]
         public async Task<ActionResult> Index()
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Curriculos> VgaInfo = new List<Curriculos>();
 
-            //mandando o request para achar os recursos de GetVaga usando HttpClient
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            //mandando o request para achar os recursos de GetCurriculos usando HttpClient
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Curriculos");
 
             //Checando a resposta se obteve sucesso ou não
             if (Res.IsSuccessStatusCode)
@@ -28,20 +27,20 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Curriculos>>(VgaResponse);
             }
 
             //Retornando a lista de vagas para a view
             return View(VgaInfo);
         }
 
-        public async Task<ActionResult<Vagas>> GetById(int id)
+        public async Task<ActionResult<Curriculos>> GetById(int id)
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Curriculos> VgaInfo = new List<Curriculos>();
 
-            Vagas vgEdit = new Vagas();
+            Curriculos vgEdit = new Curriculos();
 
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Curriculos");
 
             if (Res.IsSuccessStatusCode)
             {
@@ -49,11 +48,11 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Curriculos>>(VgaResponse);
             }
-            foreach (Vagas item in VgaInfo)
+            foreach (Curriculos item in VgaInfo)
             {
-                if (id == item.id_vaga)
+                if (id == item.id_curriculo)
                 {
                     vgEdit = item;
                 }
@@ -66,12 +65,12 @@ namespace Rh_Web_app.Controllers
 
         public ActionResult Create()
         {
-                return View(); 
+            return View();
         }
         [HttpPost]
-        public ActionResult Create(Vagas item)
+        public ActionResult Create(Curriculos item)
         {
-            var postTask = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Vagas", item);
+            var postTask = GlobalVariables.WebApiClient.PostAsJsonAsync("api/Curriculos", item);
             postTask.Wait();
 
             var result = postTask.Result;
@@ -84,13 +83,13 @@ namespace Rh_Web_app.Controllers
             return View(item);
         }
 
-        public async Task<ActionResult<Vagas>> Edit(int id)
+        public async Task<ActionResult<Curriculos>> Edit(int id)
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Curriculos> VgaInfo = new List<Curriculos>();
 
-            Vagas vgEdit = new Vagas();
+            Curriculos vgEdit = new Curriculos();
 
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Curriculos");
 
             if (Res.IsSuccessStatusCode)
             {
@@ -98,11 +97,11 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Curriculos>>(VgaResponse);
             }
-            foreach (Vagas item in VgaInfo)
+            foreach (Curriculos item in VgaInfo)
             {
-                if (id == item.id_vaga)
+                if (id == item.id_curriculo)
                 {
                     vgEdit = item;
                     break;
@@ -111,11 +110,11 @@ namespace Rh_Web_app.Controllers
 
             return View(vgEdit);
         }
-       
 
-        public async Task<ActionResult<Vagas>> Save(Vagas item)
+
+        public async Task<ActionResult<Curriculos>> Save(Curriculos item)
         {
-            var res = GlobalVariables.WebApiClient.PutAsJsonAsync("api/Vagas", item);
+            var res = GlobalVariables.WebApiClient.PutAsJsonAsync("api/Curriculos", item);
             res.Wait();
 
             var result = res.Result;
@@ -126,27 +125,27 @@ namespace Rh_Web_app.Controllers
             }
 
             ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-            return View(Edit(item.id_vaga));  
+            return View(Edit(item.id_curriculo));
         }
 
 
-        public async Task<ActionResult<Vagas>> Delete(int id) 
+        public async Task<ActionResult<Curriculos>> Delete(int id)
         {
-            string urlDel = "api/Vagas/";
+            string urlDel = "api/Curriculos/";
             urlDel = urlDel + id.ToString();
             var res = await GlobalVariables.WebApiClient.DeleteAsync(urlDel);
 
 
             return RedirectToAction("Index");
         }
-        
-        public async Task<ActionResult<Vagas>> Details(int id)
+
+        public async Task<ActionResult<Curriculos>> Details(int id)
         {
-            List<Vagas> VgaInfo = new List<Vagas>();
+            List<Curriculos> VgaInfo = new List<Curriculos>();
 
-            Vagas vgView = new Vagas();
+            Curriculos vgView = new Curriculos();
 
-            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Vagas");
+            HttpResponseMessage Res = await GlobalVariables.WebApiClient.GetAsync("api/Curriculos");
 
             if (Res.IsSuccessStatusCode)
             {
@@ -154,11 +153,11 @@ namespace Rh_Web_app.Controllers
                 var VgaResponse = Res.Content.ReadAsStringAsync().Result;
 
                 //Desserializando a resposta e a armazenando na lista de Vagas
-                VgaInfo = JsonConvert.DeserializeObject<List<Vagas>>(VgaResponse);
+                VgaInfo = JsonConvert.DeserializeObject<List<Curriculos>>(VgaResponse);
             }
-            foreach (Vagas item in VgaInfo)
+            foreach (Curriculos item in VgaInfo)
             {
-                if (id == item.id_vaga)
+                if (id == item.id_curriculo)
                 {
                     vgView = item;
                 }
@@ -166,8 +165,5 @@ namespace Rh_Web_app.Controllers
 
             return View(vgView);
         }
-
-
-    }  
+    }
 }
-
